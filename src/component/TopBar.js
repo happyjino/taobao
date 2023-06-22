0764f5b7d6f04ff6cf86d9c64291185c241ac92d
happyjino <wlsgh1128@naver.com>
2023-06-23 00:42:21 +0900

    카카오 광고 최종수정

--- src/component/TopBar.js
+++ src/component/TopBar.js
@@ -12,6 +12,21 @@ const TopBar = () => {
     hobby: "취미완구"
   }
 
+  useEffect(()=>{
+    let ins = document.createElement('ins');
+    let scr = document.createElement('script');
+    ins.className = 'kakao_ad_area';
+    ins.style = "display:none; width:100%;";
+    scr.async = 'true';
+    scr.type = "text/javascript";
+    scr.src = "//t1.daumcdn.net/kas/static/ba.min.js";
+    ins.setAttribute('data-ad-width','320');
+    ins.setAttribute('data-ad-height','50');
+    ins.setAttribute('data-ad-unit','DAN-H6lF6kPLLCXN0yti');
+    document.querySelector('.adfit').appendChild(ins);
+    document.querySelector('.adfit').appendChild(scr);
+  }, [])
+
 
   return (
     <div className="top-bar">
--- src/page/Home.js
+++ src/page/Home.js
@@ -2,21 +2,6 @@ import { useEffect } from "react";
 import TopBar from "../component/TopBar";
 
 const Home = () => {
-
-  useEffect(()=>{
-    let ins = document.createElement('ins');
-    let scr = document.createElement('script');
-    ins.className = 'kakao_ad_area';
-    ins.style = "display:none; width:100%;";
-    scr.async = 'true';
-    scr.type = "text/javascript";
-    scr.src = "//t1.daumcdn.net/kas/static/ba.min.js";
-    ins.setAttribute('data-ad-width','160');
-    ins.setAttribute('data-ad-height','600');
-    ins.setAttribute('data-ad-unit','DAN-H6lF6kPLLCXN0yti');
-    document.querySelector('.adfit').appendChild(ins);
-    document.querySelector('.adfit').appendChild(scr);
-  }, [])
   
   return (
     <div className="home">
