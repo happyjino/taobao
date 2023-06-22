254c5e9bc872a73797766496f0357b1b71cb80bd
happyjino <wlsgh1128@naver.com>
2023-06-23 00:51:10 +0900

    카카오 광고 최최최종

--- src/page/Home.js
+++ src/page/Home.js
@@ -2,6 +2,20 @@ import { useEffect } from "react";
 import TopBar from "../component/TopBar";
 
 const Home = () => {
+  useEffect(()=>{
+    let ins = document.createElement('ins');
+    let scr = document.createElement('script');
+    ins.className = 'kakao_ad_area';
+    ins.style = "display:none; width:100%;";
+    scr.async = 'true';
+    scr.type = "text/javascript";
+    scr.src = "//t1.daumcdn.net/kas/static/ba.min.js";
+    ins.setAttribute('data-ad-width','320');
+    ins.setAttribute('data-ad-height','50');
+    ins.setAttribute('data-ad-unit','DAN-IKW5Iw58dB6XvXdp');
+    document.querySelector('.adfit2').appendChild(ins);
+    document.querySelector('.adfit2').appendChild(scr);
+  }, [])
   
   return (
     <div className="home">
@@ -17,7 +31,8 @@ const Home = () => {
         <p>- 쇼핑몰 옆의 설명은 임의로 적었어요</p>
         <p>- 맞지 않는 정보나 원하는 기능, 문의사항은 아래 메일로 주세요</p>
         <p>hahainfo99@gmail.com</p>
-      </div>      
+      </div>
+      <div className="adfit2" />
     </div>
   )
 }
