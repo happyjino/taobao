c774b97765c0a5c0945742376fed45c4fa97244e
happyjino <wlsgh1128@naver.com>
2023-06-23 00:31:33 +0900

    version_2 광고추가

--- src/page/ShopList.js
+++ src/page/ShopList.js
@@ -27,6 +27,21 @@ const ShopList = () => {
     setRank('silver')
   }
 
+  useEffect(()=>{
+    let ins = document.createElement('ins');
+    let scr = document.createElement('script');
+    ins.className = 'kakao_ad_area';
+    ins.style = "display:none; width:100%;";
+    scr.async = 'true';
+    scr.type = "text/javascript";
+    scr.src = "//t1.daumcdn.net/kas/static/ba.min.js";
+    ins.setAttribute('data-ad-width','160');
+    ins.setAttribute('data-ad-height','600');
+    ins.setAttribute('data-ad-unit','DAN-H6lF6kPLLCXN0yti');
+    document.querySelector('.adfit').appendChild(ins);
+    document.querySelector('.adfit').appendChild(scr);
+  }, [])
+
   return (
     <div className="shop-list">
       <TopBar />
@@ -60,6 +75,7 @@ const ShopList = () => {
           })}
         </table>
       </div>
+      <div className="adfit" />
     </div>
   )
 }
